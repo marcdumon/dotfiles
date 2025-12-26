bfbd57ed7c0d6df7c68f85a33618fbca4f672957
Marc Dumon <7793950+marcdumon@users.noreply.github.com>
2025-12-26 01:30:38 +0100

    fix: correct source and destination

--- scripts/scripts/copy_timeshift_to_external_drive.sh
+++ scripts/scripts/copy_timeshift_to_external_drive.sh
@@ -1,1 +1,1 @@
-sudo rsync -aAXHv --ignore-existing --numeric-ids /mnt/Backup/timeshift/ /media/md/Linux_Files/timeshift
+sudo rsync -aAXHvL --ignore-existing --numeric-ids /mnt/Backup/timeshift/snapshots-ondemand/ /media/md/Linux_Files/timeshift/snapshots-ondemand/
