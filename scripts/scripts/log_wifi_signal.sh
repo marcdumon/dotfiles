8fec9e36cae4baab0bec2ca77af1c1c50a60bd74
Marc Dumon <7793950+marcdumon@users.noreply.github.com>
2026-03-30 18:57:13 +0200

    improved log_wifi_signal.sh

--- scripts/scripts/log_wifi_signal.sh
+++ scripts/scripts/log_wifi_signal.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-for i in $(seq 150); do echo "$(date +%T) $(iw dev wlp1s0 link | grep -oP 'signal: \K[^ ]+') dBm"; sleep 2; done | tee ./temp/wifi_signal.log
+echo "Logs wifi signal strength (dBm) to ~/temp/wifi_signal.log"
+for i in $(seq 150); do echo "$(date '+%Y-%m-%d %H:%M:%S') $(iw dev wlp1s0 link | grep -oP 'signal: \K[^ ]+') dBm"; sleep 2; done | tee -a ~/temp/wifi_signal.log
