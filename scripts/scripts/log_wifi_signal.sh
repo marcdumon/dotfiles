1963a2466b0bc731cabeed3ab2a3a67d75f09715
Marc Dumon <7793950+marcdumon@users.noreply.github.com>
2026-03-31 09:32:32 +0200

    improved log_wifi_signal: loop forever and 1 sec interval

--- scripts/scripts/log_wifi_signal.sh
+++ scripts/scripts/log_wifi_signal.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Logs wifi signal strength (dBm) to ~/temp/wifi_signal.log"
-for i in $(seq 150); do echo "$(date '+%Y-%m-%d %H:%M:%S') $(iw dev wlp1s0 link | grep -oP 'signal: \K[^ ]+') dBm"; sleep 2; done | tee -a ~/temp/wifi_signal.log
+while true; do echo "$(date '+%Y-%m-%d %H:%M:%S') $(iw dev wlp1s0 link | grep -oP 'signal: \K[^ ]+') dBm"; sleep 1; done | tee -a ~/temp/wifi_signal.log
